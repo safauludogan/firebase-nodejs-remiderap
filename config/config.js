673021477e7ac6cab169a4524988f1edd085d3b0
Safa <suludogan95@gmail.com>
2023-03-03 15:21:25 +0300

    firebase init

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const firebase = require("firebase/compat/app");
 require("firebase/compat/auth");
 require("firebase/compat/firestore");
-var serviceAccount = require("../serviceAccountKey.json");
+//var serviceAccount = require("../serviceAccountKey.json");
 const firebaseConfig = {
     apiKey: "AIzaSyCH45w1FIc8u8Yrk--9kMN8F0vR8eCVb8w",
     authDomain: "reminder-app-73eac.firebaseapp.com",
